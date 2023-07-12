5004fbb4b1c6d5b8a574cbd4fcda7e8b8b052dd6
0z0z0z0z0z <139330525+Bogotvorec@users.noreply.github.com>
2023-07-12 16:28:35 +0300

    zxxzxzx

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup
 from dotenv import load_dotenv
 import os
 
@@ -6,14 +7,52 @@ load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('Функції').add('Взяти тачку').add('Сдати звіт').add('Політика')
 
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('Функції').add('Взяти тачку').add('Сдати звіт').add('Політика').add('Адмін-панель')
+
+admin_panel = ReplyKeyboardMarkup(resize_keyboard=True)
 
 @dp.message_handler(commands=['start'])
 async def cmd_stat(message: types.message):
-    await message.answer (f'{message.from_user.first_name} Шо ти голова?')
+    await message.answer_sticker('CAACAgIAAxkBAAMdZK6VIgzmTEVQzdkYOqLA91KY5tcAAhoAA8A2TxOC27C1PAZBVy8E')
+    await message.answer (f'{message.from_user.first_name} Пиздуй отсюда',
+                          reply_markup=main)
+
+@dp.message_handler(text='Взяти тачку')
+async def takecar(massage: types.Message):
+    await massage.answer(f'Машину подано Cер! , ви можете взять ключи в оговоренном месте')
+
+@dp.message_handler(text='Сдати звіт')
+async def takecar(massage: types.Message):
+    await massage.answer(f'Машину подано Cер! , ви можете взять ключи в оговоренном месте')
+
+@dp.message_handler(text='Функції')
+async def takecar(massage: types.Message):
+    await massage.answer(f'Машину подано Cер! , ви можете взять ключи в оговоренном месте')
 
 
-@dp.message_handler()
+@dp.message_handler(text='Політика')
+async def takecar(massage: types.Message):
+    await massage.answer(f'Машину подано Cер! , ви можете взять ключи в оговоренном месте')
+
+
+#@dp.message_handler(content_types=['start'])
+#async def chek_stiker(messege: types.Message):
+#    await messege.answer_sticker('CAACAgIAAxkBAAMdZK6VIgzmTEVQzdkYOqLA91KY5tcAAhoAA8A2TxOC27C1PAZBVy8E')
+#    await messege.answer(f'{messege.form_user.first_name}, Пиздуй отсюда ')
+
+@dp.message_handler(content_types=['sticker'])
+async def chek_stiker(message: types.Message):
+    await message.answer(message.sticker.file_id)
+    await bot.send_message(message.from_user.id,message.chat.id)
+
+@dp.message_handler(content_types=['document', 'photo'])
+async def forward_message(message: types.Message):
+    await bot.forward_message(os.getenv('GROUP_ID'), message.from_user.id,message.message_id)
+
 async def answer(message: types.Message):
     await message.reply('Ты втираешь мне дичь')
 
